63c622e2d1f3dc6f5321257b79cce8a23248cfc8
MikkelKSpecialisterne <mikkel.kristensen@specialisterne.dk>
2026-09-18 10:16:30 +0200

    getting a gui that works without visual cards

--- Control.py
+++ Control.py
@@ -22,15 +22,24 @@ class GameController:
 
         if event.type == pygame.MOUSEBUTTONDOWN:
             pos = event.pos
-            # TODO: check pos against self.view's button rects,
-            # call the matching method on self.game
+            if self.view.hit_button.collidepoint(pos) and self.game.game_over==False:
+                self.game.player.hit(self.game.deck)
+            if self.view.double_button.collidepoint(pos) and self.game.game_over==False:
+                self.game.player.hit(self.game.deck)
+                self.game.player.stand()
+            if self.view.stand_button.collidepoint(pos) and self.game.game_over==False:
+                self.game.player.stand()
+            if self.view.new_game_button.collidepoint(pos) and self.game.game_over== True:
+                self.game.new_game()
             pass
 
     def run(self):
         while self.running:
             for event in pygame.event.get():
                 self.handle_event(event)
-
+            if (self.game.player.passed or self.game.player.hand.value_in_hand>21 and self.game.game_over == False):
+                self.game.dealer.play(self.game.deck)
+                self.game.check_victory()
             self.view.draw(self.game)
             self.clock.tick(60)
 
--- View.py
+++ View.py
@@ -1,6 +1,6 @@
 import pygame
 
-WIDTH, HEIGHT = 800, 600
+WIDTH, HEIGHT = 1000, 800
 
 
 class GameView:
@@ -8,8 +8,11 @@ class GameView:
         self.screen = screen
         self.font = pygame.font.SysFont("arial", 22)
 
-        # TODO: define any Rects you need for buttons here, e.g.
-        # self.hit_button = pygame.Rect(300, 520, 100, 45)
+        self.hit_button = pygame.Rect(300, 520, 100, 45)
+        self.double_button = pygame.Rect(420, 520, 100, 45)
+        self.stand_button = pygame.Rect(540, 520, 100, 45)
+        self.new_game_button = pygame.Rect(0, 0, 130, 45)
+        self.new_game_button.center = (WIDTH // 2, 400)
 
     def draw(self, game):
         """Called once per frame. Take a model object, draw its current state."""
@@ -18,9 +21,27 @@ class GameView:
         # TODO: draw dealer's hand
         # TODO: draw player's hand
         # TODO: draw buttons / prompts depending on game state
-        text_surface = self.font.render("Your hand: "+ game.convert_player_hand(game.player.hand)+ ". Total value: " + str(game.player.hand.value_in_hand), True, (255, 255, 255))
-        self.screen.blit(text_surface, (100, 400))
-        text_surface = self.font.render("Dealer hand: "+ game.convert_player_hand(game.dealer.hand)+ ". Total value: " + str(game.dealer.hand.value_in_hand), True, (255, 255, 255))
-        self.screen.blit(text_surface, (100, 50))
+        player_text = self.font.render("Your hand: "+ game.convert_player_hand(game.player.hand)+ ". Total value: " + str(game.player.hand.value_in_hand), True, (255, 255, 255))
+        self.screen.blit(player_text, (150, 600))
+        if (game.game_over == False):
+            dealer_text = self.font.render("Dealer hand: "+ game.convert_dealer_hand(game.dealer.hand), True, (255, 255, 255))
+            self.screen.blit(dealer_text, (150, 50))
+            pygame.draw.rect(self.screen, (90, 90, 90), self.hit_button)
+            pygame.draw.rect(self.screen, (90, 90, 90), self.double_button)
+            pygame.draw.rect(self.screen, (90, 90, 90), self.stand_button)
+            hit_label = self.font.render("Hit", True, (255, 255, 255))
+            self.screen.blit(hit_label, hit_label.get_rect(center=self.hit_button.center))
+            double_label = self.font.render("Double", True, (255, 255, 255))
+            self.screen.blit(double_label, double_label.get_rect(center=self.double_button.center))
+            stand_label = self.font.render("Stand", True, (255, 255, 255))
+            self.screen.blit(stand_label, stand_label.get_rect(center=self.stand_button.center))
+        elif (game.game_over == True):
+            victory_text = self.font.render(game.victory_text, True, (255,255,255))
+            self.screen.blit(victory_text, victory_text.get_rect(center=(WIDTH // 2, 350)))
+            dealer_text = self.font.render("Dealer hand: "+ game.convert_player_hand(game.dealer.hand)+ ". Total value: " + str(game.dealer.hand.value_in_hand), True, (255, 255, 255))
+            self.screen.blit(dealer_text, (150, 50))
+            pygame.draw.rect(self.screen, (90,90,90), self.new_game_button)
+            new_game_label = self.font.render("New Game", True, (255,255,255))
+            self.screen.blit(new_game_label, new_game_label.get_rect(center=self.new_game_button.center))
 
         pygame.display.flip()
